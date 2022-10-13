a8c5058b29c5e93e825836c77b18954a067aee6a
Fahrul Rozi M <fahrulrozimeiduan@gmail.com>
2022-10-13 16:11:30 +0000

    add data source

---
+++ final-project-dbt/models/final-project-data/src_fraud_data.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized="view"
+    )
+}}
+with fraud_dataset_src as (
+    select
+        *
+    from
+        {{ source('final_project', 'fraud_dataset_final') }}
+)
+select
+    step,
+    type,
+    amount,
+    senderID,
+    oldbalanceSend,
+    newbalanceSend,
+    difsender,
+    ReceiverID,
+    oldbalancereceive,
+    newbalancereceive,
+    difreceiver,
+    isFraud,
+    isFlaggedFraud,
+    datetime
+from fraud_dataset_src
+order by datetime ASC;
